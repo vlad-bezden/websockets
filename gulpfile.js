0ce1b9d98b27b3e66634fc4a8042a1ca895a58d6
Vlad Bezden <vlad.bezden@gmail.com>
2016-01-19 13:25:55 -0500

    Added more tasks and cleaned up tasks.json so it has only build command and all tasks are in a gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 var gulp = require('gulp'),
-	webserver = require('gulp-webserver');
+    webserver = require('gulp-webserver');
 
+// Serves static pagis with auto reload
 gulp.task('webserver', function () {
-	gulp.src('public')
-		.pipe(webserver({
-			livereload: true,
-			open: true
-		}));
+    gulp.src('public')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
 });
 
+// Serves static pages without auto reloading
+gulp.task('run', function () { 
+    gulp.src('public')
+        .pipe(webserver({
+            open: true
+        }));
+});
+
+// stops web server
 gulp.task('kill', function () {
-	gulp.src('public')
-		.pipe(webserver())
-		.emit('kill');
+    gulp.src('public')
+        .pipe(webserver())
+        .emit('kill');
 });
 
 // default gulp task that will be executed from command prompt if no tasks are specified
